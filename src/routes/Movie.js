d6e20519c444538c23e3b076eb87d258047b807e
jonghyeon <whdgus003@gmail.com>
2022-09-11 17:47:44 +0900

    디자인 추가, like & apollo client 캐시를 이용한 like & unlike 기능 추가

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,94 @@
-export default function Movie(){
-  return <div>this is movie details</div>;
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Image = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      medium_cover_image
+      rating
+      isLiked @client
+    }
+  }
+`;
+export default function Movie() {
+  const params = useParams();
+
+  const {
+    data,
+    loading,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: params.id,
+    },
+  });
+
+  const onClick = () => {
+    cache.writeFragment({
+      id: `Movie:${params.id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          isLiked
+        }
+      `,
+      data: {
+        isLiked: data.movie.isLiked ? false : true,
+      },
+    });
+  };
+
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+        <button onClick={onClick}>
+          {data?.movie?.isLiked ? "Unlike" : "Like"}
+        </button>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image} />
+    </Container>
+  );
 }
